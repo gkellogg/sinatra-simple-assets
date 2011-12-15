233b81d2c290652931eefb30c9cebcac0b25dcb4
Pete O'Grady <pete@peteogrady.com>
2011-12-15 08:25:34 +0000

    Initial commit

---
+++ lib/sinatra-simple-assets.rb
@@ -0,0 +1,9 @@
+require "sinatra-simple-assets/version"
+
+module Sinatra
+  module Simple
+    module Assets
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/sinatra-simple-assets/version.rb
@@ -0,0 +1,7 @@
+module Sinatra
+  module Simple
+    module Assets
+      VERSION = "0.0.1"
+    end
+  end
+end
