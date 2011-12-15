5b91c07e713bf51139a1a219658ee5745896ea2e
Pete O'Grady <pete@peteogrady.com>
2011-12-15 08:54:44 +0000

    DSL structure

--- lib/sinatra/simple_assets.rb
+++ lib/sinatra/simple_assets.rb
@@ -3,5 +3,22 @@ require 'sinatra/base'
 module Sinatra
   module SimpleAssets
     VERSION = '0.0.1'
+
+    class Assets
+      def initialize(app, &block)
+        @app = app
+        instance_eval(&block)
+      end
+
+      def css(name, files)
+      end
+
+      def js(name, files)
+      end
+    end
+
+    def assets(&block)
+      @assets ||= Assets.new(self, &block)
+    end
   end
 end
